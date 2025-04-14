bd7debef85285f26e1dbeb49e9d3361f02931c6a
Batsirai Muchareva <batsiraim@elementor.red>
2025-04-14 16:37:41 +0200

    wip

--- app/components/loader.tsx
+++ app/components/loader.tsx
@@ -4,7 +4,7 @@ export const Loader = () => (
     <Fragment>
         <video className="video-loader" autoPlay loop muted playsInline>
             <source src="assets/img/loading/glory-today.mp4" type="video/mp4"/>
-            <p>Testing</p>
+            <p>remove</p>
         </video>
     </Fragment>
 );
