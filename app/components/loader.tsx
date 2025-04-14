dfd6bd26004aeed3a7f8138239ba345cd155c64f
Batsirai Muchareva <162136788+Batsirai-muchareva@users.noreply.github.com>
2025-04-14 16:36:49 +0200

    test

--- app/components/loader.tsx
+++ app/components/loader.tsx
@@ -4,6 +4,7 @@ export const Loader = () => (
     <Fragment>
         <video className="video-loader" autoPlay loop muted playsInline>
             <source src="assets/img/loading/glory-today.mp4" type="video/mp4"/>
+            <p>Testing</p>
         </video>
     </Fragment>
 );
